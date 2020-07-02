4cb9427dada015bccec6bb48371b5e2403a4529a
Dandan <ddliu@thoughtworks.com>
2020-07-02 19:53:18 +0800

    implement practice

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,17 @@
-const parseData
+const parseData = (input) => {
+    const data = input.data;
+    const column = input.column;
+
+    let result = [];
+    data.forEach(d => {
+        const obj = {};
+        column.forEach((c, idx) =>{
+            obj[c.name] = d[idx];
+        });
+
+        result.push(obj);
+    });
+    return result;
+};
+
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,7 @@
-const inject
+const inject = (items, sections) => {
+    sections.forEach((s, idx) => {
+        items.splice(s.index+idx, 0, s.content);
+    });
+    return items;
+}
 export { inject };
